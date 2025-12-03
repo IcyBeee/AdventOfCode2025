1ba8899ab8da79bd7a929d3f4a17d98370352f9c
Ian Biles <ian@biles.plus.com>
2025-12-03 11:58:09 +0000

    Added comments and renamed some variables

--- Day03 Lobby/Part2.py
+++ Day03 Lobby/Part2.py
@@ -5,10 +5,13 @@ with (open("input.txt", "r") as inf):
         value = 0
         for batteries_remaining in range(no_batteries_required, 0, -1):
             no_batteries_available = len(batteries)
-            truncated = batteries[:-batteries_remaining]
-            highest = max([int(x) for x in truncated])
-            first = batteries.index(str(highest))
-            batteries = batteries[first+1:]
+            # The next digit cannot come from the last "batteries_remaining" digits
+            truncated_batteries = batteries[:-batteries_remaining]
+            highest = max([int(x) for x in truncated_batteries])
+            # Find the earliest occurrence of the highest digit
+            first_index = batteries.index(str(highest))
+            # Strip everything before this digit from the battery list
+            batteries = batteries[first_index + 1:]
             value = (value*10) + highest
         sum += value
 
