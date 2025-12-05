06f0a0abf57c000cae6a50b379ca5038dd62650b
Ian Biles <ian@biles.plus.com>
2025-12-05 08:49:01 +0000

    First solution for day 5. Part 2 can wait!

---
+++ Day05 Cafeteria/Part1.py
@@ -0,0 +1,20 @@
+import re
+
+ranges = list()
+total = 0
+
+with open("input.txt", "r") as inf:
+    while line := inf.readline().rstrip():
+        if match := re.match(r"(\d+)-(\d+)", line):
+            ranges.append((int(match[1]), int(match[2])))
+
+    while line := inf.readline().rstrip():
+        val = int(line)
+        fresh = False
+        for minimum, maximum in ranges:
+            if minimum <= val <= maximum:
+                fresh = True
+                total += 1
+                break
+
+print(total)
